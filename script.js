d00621ad5b5b488d5facbbf66c1b390f977024a8
LuisATCastro <130671483+LuisATCastro@users.noreply.github.com>
2023-12-08 18:37:14 -0300

    atividade pw

--- script.js
+++ script.js
@@ -1,24 +1,23 @@
-
 function setTheme(theme) {
-    if (theme === 'dark') {
-      $('body').addClass('dark-mode');
-    } else {
-      $('body').removeClass('dark-mode');
-    }
+  if (theme === 'dark') {
+    $('body').addClass('dark-mode');
+  } else {
+    $('body').removeClass('dark-mode');
   }
-  
-  $('.btn-group .btn').on('click', function () {
-    const theme = $(this).data('theme');
+}
+
+$('.btn-group .btn').on('click', function () {
+  const theme = $(this).data('theme');
+  setTheme(theme);
+
+  localStorage.setItem('theme', theme);
+});
+
+$(document).ready(function () {
+  const theme = localStorage.getItem('theme');
+  if (theme) {
     setTheme(theme);
-  
-    localStorage.setItem('theme', theme);
-  });
-  
-  $(document).ready(function () {
-    const theme = localStorage.getItem('theme');
-    if (theme) {
-      setTheme(theme);
-  
-      $(`.btn-group .btn[data-theme="${theme}"]`).addClass('selected-theme');
-    }
-  });
+
+    $(`.btn-group .btn[data-theme="${theme}"]`).addClass('selected-theme');
+  }
+});
